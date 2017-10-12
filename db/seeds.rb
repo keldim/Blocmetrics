e87ec4a7352c4d0c596f72cf3b857ee662d0baf1
Chris Yoo <woyoo@umich.edu>
2017-10-12 04:09:27 -0400

    event model created, database is seeded, show view is modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do
+   User.create!(
+   email:    Faker::Internet.unique.email,
+   password: Faker::Internet.unique.password(8)
+   )
+ end
+
+users = User.all
+
+ 5.times do
+    Application.create!(
+    name:    Faker::Company.unique.name,
+    url: Faker::Internet.unique.url,
+    user: users.sample
+    )
+  end
+
+applications = Application.all
+
+  20.times do
+     Event.create!(
+     name:    Faker::Company.unique.name,
+     application: applications.sample
+     )
+   end
+
+
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Application.count} applications created"
+puts "#{Event.count} events created"
