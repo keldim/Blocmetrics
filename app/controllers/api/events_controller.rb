c1871537d0d52512ee764e6cdfd08fc410a44c70
Chris Yoo <woyoo@umich.edu>
2017-10-13 00:52:59 -0400

    revised api/events_controller, finished client-side javascript 7

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,16 +1,16 @@
 class API::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_filter :set_access_control_headers
+  before_action :set_access_control_headers
 
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Content-Type'
   end
-  
+
 
   def create
-    @registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+    @registered_application = Application.find_by(url: request.env['HTTP_ORIGIN'])
 
     unless @registered_application.present?
       render json: "Unregistered application", status: :unprocessable_entity
